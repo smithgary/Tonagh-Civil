1e0f11f33b82c3b3edf526ddef2e9d6f01517ec9
Gary Smith <gary@meteorsoftware.co.uk>
2020-12-02 20:51:34 +0000

    Initial Commit

---
+++ Tonagh Civil/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Tonagh Civil
+//
+//  Created by Gary Smith on 02/12/2020.
+//  Copyright © 2020 Sperringold. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
